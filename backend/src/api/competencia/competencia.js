dfcba47bbcb8134ae98f8e981e125b6995534957
Renê Milagres <renevictorm@gmail.com>
2018-04-17 15:16:29 -0300

    competencia.js pronta

--- backend/src/api/competencia/competencia.js
+++ backend/src/api/competencia/competencia.js
@@ -1,1 +1,11 @@
-fdsa
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+
+const competenciaSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    observacao: { type: String, required: true },
+   
+})
+
+module.exports = restful.model('Competencia', competenciaSchema)
