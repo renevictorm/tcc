f8bdd69b934e5527a54f0c3972ab3e2f98dbec75
Renê Milagres <renevictorm@gmail.com>
2018-04-24 15:04:11 -0300

    projeto.js feito

---
+++ backend/src/api/projeto/projeto.js
@@ -0,0 +1,22 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+
+const projetoSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    situacao: {type: String, required: false},
+    inicio: { type: Date, required: true },
+    fim: { type: Date, required: true },
+    inicioEsperado: { type: Date, required: true },
+    fimEsperado: { type: Date, required: true },
+    descricao: { type: String, required: true },
+    precoReal: { type: String, required: true },
+    dprecoEsperado: { type: String, required: true },
+    descricao: { type: String, required: true },
+    gastos: {type: String, required:false},
+    primeiroContato:{type: Date, required:false}
+    
+   
+})
+
+module.exports = restful.model('Fase', projetoSchema)
