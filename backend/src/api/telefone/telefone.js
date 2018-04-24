0f8e31451eebbb189d6c0f7026066d9dfea76a56
Renê Milagres <renevictorm@gmail.com>
2018-04-24 15:17:53 -0300

    telefone.js criado

---
+++ backend/src/api/telefone/telefone.js
@@ -0,0 +1,10 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+
+const telefoneSchema = new mongoose.Schema({
+    numero: { type: String, required: true },
+  
+})
+
+module.exports = restful.model('Fase', telefoneSchema)
