0cdd9cf49e53ac4f3c01eadc23a6d690b6047952
Renê Milagres <renevictorm@gmail.com>
2018-05-08 15:41:24 -0300

    Colaborador Horario no Backend

---
+++ backend/src/api/ColaboradorHorario/ch.js
@@ -0,0 +1,12 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+
+const chSchema = new mongoose.Schema({
+    
+    presenca: { type: Boolean, required: true, default: false },
+    data: { type: Date, required: true },
+    
+})
+
+module.exports = restful.model('Ch', chSchema)
