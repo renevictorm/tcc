7becc8226816096ca464f8ce90a58c270a216706
Renê Milagres <renevictorm@gmail.com>
2018-05-30 09:53:39 -0300

    integração mais delete

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -13,16 +13,33 @@ export default class Todo extends Component {
         this.state = { description: '', list: [] }
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
     }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
+    }
+
+
     handleChange(e) {
         this.setState({ ...this.state, description: e.target.value })
     }
     handleAdd() {
         const description = this.state.description
-        axios.post(URL, { description: description, done: true })
-            .then(resp => console.log('Funcionou!'))
-
+        axios.post(URL, { description })
+            .then(resp => this.refresh())
     }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+
+
+
+    
     render() {
         return (
             <div>
@@ -30,7 +47,9 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} />
-                <TodoList />
+                    
+                <TodoList list={this.state.list}
+                handleRemove={this.handleRemove} />
             </div>
         )
     }
