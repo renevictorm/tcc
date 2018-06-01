a304fabb91df59bcbee7947124e1bc02cacae8f0
Renê Milagres <renevictorm@gmail.com>
2018-06-01 12:15:46 -0300

    começando a integração com o redux

--- frontend/src/Index.jsx
+++ frontend/src/Index.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './main/app'
-
-ReactDOM.render(<App />, document.getElementById('app'))
+import reducers from './main/reducers'
+const store = createStore(reducers)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
