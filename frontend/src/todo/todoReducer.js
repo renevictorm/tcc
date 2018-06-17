5a56401207628719a742d5fc38156c3708f0e819
Renê Milagres <renevictorm@gmail.com>
2018-06-17 13:36:11 -0300

    Actions de pesquisa e todo e correção de problemas

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,25 +1,16 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura do cartão',
-        done: true
-    },
-    {
-        _id: 2,
-        description: 'Reunião com a equipe às 10:00',
-        done: false
-    },
-    {
-        _id: 3,
-        description: 'Consulta médicas na terça depois do almoço',
-        done: false
-    }]
+    description: '',
+    list: []
 }
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data }
+        case 'TODO_ADDED':
+            return { ...state, description: '' }
+
         default:
             return state
     }
